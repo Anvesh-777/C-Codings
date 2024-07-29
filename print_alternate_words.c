8817d5cd2b9970fdb54f4298456872923e40e612
༒☬AnvesH☬༒ <132336499+Anvesh-777@users.noreply.github.com>
2024-07-29 12:54:15 +0530

    Add files via upload
    Alternate Words Printing

---
+++ print_alternate_words.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main4()
+{
+    const char *str = "  abd   def   ght      ydr  udf    ffe  ";
+    int word_count = 0;
+    while (*str != '\0')
+    {
+        // if there space is available first only ...Skip leading spaces
+        while (*str != '\0' && (*str == ' ' || *str == '\t' || *str == '\n'))
+        {
+            str++;
+        }
+        // If we are at the beginning of a word
+        if (*str != '\0' && !(*str == ' ' || *str == '\t' || *str == '\n'))
+        {
+            word_count++;
+            // Print the word if it is in an odd position (1st, 3rd, 5th, etc.)
+            if (word_count % 2 != 0)
+            {
+                // Print the word
+                while (*str != '\0' && !(*str == ' ' || *str == '\t' || *str == '\n'))
+                {
+                    printf("%c",*str);
+                    str++;
+                }
+                printf("\n");
+            }
+            else
+            {
+                // Skip the word
+                while (*str != '\0' && !(*str == ' ' || *str == '\t' || *str == '\n'))
+                {
+                    str++;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
