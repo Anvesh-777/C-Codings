12dfe054bca9dc06aa06b8ce1ad2b0e6c53b4678
༒☬AnvesH☬༒ <132336499+Anvesh-777@users.noreply.github.com>
2024-07-30 22:51:26 +0530

    Update Character_frequencies.c
    without using string handling functions

--- Character_frequencies.c
+++ Character_frequencies.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() 
 {
     char *inputString="JjjjjjjAaaaaiiiiI";
     int count[256] = {0};  
-    for (int i = 0; i < strlen(inputString); i++)
+    int string_length=0,i=0;
+    
+    //finding string length without using string handling function i.e strlen()
+    while(inputString[i++])
+    string_length++;
+    printf("The string length=%d\n",string_length);
+    
+ 
+    for (int i = 0; i < string_length; i++)
     {
         count[(int)inputString[i]]++;
     }
+    
     printf("Character frequencies:\n");
     for (int i = 0; i < 256; i++)
     {
