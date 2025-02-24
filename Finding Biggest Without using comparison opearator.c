580665d58604849d8ad5b56672bdd3dc6a9f269d
༒☬AnvesH☬༒ <132336499+Anvesh-777@users.noreply.github.com>
2025-02-24 14:58:58 +0530

    Update Finding Biggest Without using comparison opearator.c

--- Finding Biggest Without using comparison opearator.c
+++ Finding Biggest Without using comparison opearator.c
@@ -5,16 +5,19 @@ Requirement: Find Biggest of two numbers without using comparison operators
 
 #include <stdio.h>
 #include <stdlib.h>
-int main() 
+int main()
 {
-    int a=0,b=0;
-    printf("Enter two number:");
-    scanf("%d%d", &a,&b);
+	int a=0,b=0;
+	printf("Enter two number:");
+	scanf("%d%d", &a,&b);
+	if(a==b) {
+		printf("Both numbers are same");
+		return 0;
+	}
+	int big_number=(a+b+abs(a-b))/2;
 
-    int big_number=(a+b+abs(a-b))/2;
+	printf("Result = %d", big_number);
 
-    printf("Result = %d", big_number);
-
-    return 0;
+	return 0;
 }
 
